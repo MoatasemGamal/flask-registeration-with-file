2c2bacf754f391d0aca8c204ce55a0d7dc0288aa
MoatasemGamal <moatasemco@gmail.com>
2023-11-16 16:50:54 +0200

    implement required routes for auth

--- main.py
+++ main.py
@@ -4,3 +4,15 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route("/")
+def home_page():
+    return "<h1>This is The Home Page</h1>"
+
+@app.route("/login")
+def login_page():
+    return "<h1>Login</h1>"
+
+@app.route("/register")
+def register_page():
+    return "<h1>Register</h1>"
