73b30863204f28d1689519a8f45966714620a2a3
MoatasemGamal <moatasemco@gmail.com>
2023-11-16 18:48:51 +0200

    implement login and register, render home.html for GET method

--- main.py
+++ main.py
@@ -2,18 +2,30 @@
 # print(flask.__version__)
 
 from flask import Flask, render_template, request, redirect
+from Classes import User, UsersContainerSingleton, Auth
+import pyfiglet
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def home_page():
-    if request.method == 'POST' and "name" in request.form:
+    container = UsersContainerSingleton.get_instance()
+    container.load_users()
+    if request.method == 'POST' and "name" in request.form and "email" in request.form and "password" in request.form:
         # Register
-        return request.form
-    elif request.method == 'POST':
+        u = Auth.register(request.form["name"].strip(), request.form["email"].strip(), request.form["password"].strip())
+        if u:
+            return f"<pre>{pyfiglet.figlet_format("Hello "+u.name)}</pre>"
+        else:
+            return f"<pre>{pyfiglet.figlet_format("Sorry, Failed To Register")}</pre>"
+    elif request.method == 'POST' and "email" in request.form and "password" in request.form:
         #Login
-        return request.form
-    return "<h1>This is The Home Page</h1>"
+        u = Auth.login(request.form["email"].strip(), request.form["password"].strip())
+        if u:
+            return f"<pre>{pyfiglet.figlet_format("Welcome Back, "+u.name)}</pre>"
+        else:
+            return f"<pre>{pyfiglet.figlet_format("Sorry, Failed To Login")}</pre>"
+    return render_template("home.html")
 
 @app.route("/login")
 def login_page():
